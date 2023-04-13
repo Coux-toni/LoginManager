a2de68d1464aab2ec27c7b9b373bd665ae4f1d64
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2023-04-13 11:05:05 +0200

    add passwort

---
+++ Passwort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginManager
+{
+    public class Passwort
+    {
+        public string Titel { get; protected set; }
+        public string Pwd { get; protected set; }
+        public Passwort(string titel, string pwd) 
+        {
+            this.Titel = titel;
+            this.Pwd = pwd;
+        }   
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using LoginManager;
 
 Generator gen = new Generator();
+ErweiterterGenerator genExt = new ErweiterterGenerator();
+genExt.Set("Sommer");
+Console.WriteLine(gen.Generieren());
+Console.WriteLine(genExt.Generieren());
