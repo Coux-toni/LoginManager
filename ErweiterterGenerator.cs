5baba0a4fc11a1c23841deafa2fb9d8b3547722f
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2023-04-13 11:01:10 +0200

    add erweitertes passwort

---
+++ ErweiterterGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginManager
+{
+    public class ErweiterterGenerator : Generator
+    {
+        public override string Generieren()
+        {
+            Random r = new Random();
+
+            return base.Generieren() + r.Next(1,10);
+        }
+    }
+}
