41fed68e7b379e37742bc6348951e4e06ab0be2b
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2023-04-13 11:06:13 +0200

    add manager

---
+++ Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginManager
+{
+    public class Manager
+    {
+        List<Passwort> passwoerter = new();
+        public void Speichern(Passwort p)
+        {
+            this.passwoerter.Add(p);
+        }
+    }
+}
