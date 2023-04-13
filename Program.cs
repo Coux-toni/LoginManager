5bcd51854d8f72e5fa8cfb8ced74b1d74744f125
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2023-04-13 11:11:15 +0200

    objects created

--- Program.cs
+++ Program.cs
@@ -5,3 +5,8 @@ ErweiterterGenerator genExt = new ErweiterterGenerator();
 genExt.Set("Sommer");
 Console.WriteLine(gen.Generieren());
 Console.WriteLine(genExt.Generieren());
+Manager n = new();
+Passwort p1 = new("Dorpbox", genExt.Generieren());
+n.Speichern(p1);
+Passwort p2 = new("Dorpbox", genExt.Generieren());
+n.Speichern(p2);
